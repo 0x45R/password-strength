bee04f37bedbdd6e2f7312d4acf3a96aba39ca81
0x45R <robertplawski8@gmail.com>
2023-10-26 23:41:39 +0200

    Upgraded site look and feel

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
-const passwordInput = document.querySelector('.password-input');
-const passwordCharacters = document.querySelector('.password-characters')
+const showPasswordButton = document.querySelector('.js-show-password-button');
+const passwordInput = document.querySelector('.js-password-input');
+const charCountLabel = document.querySelector('.js-char-count');
+const crackTimeLabel = document.querySelector('.js-crack-time');
+const scoreLabel = document.querySelector('.js-score');
+const resultContainer = document.querySelector('.js-result');
+const suggestionsLabel = document.querySelector('.js-suggestions-label');
+const warningContainer = document.querySelector('.js-warning');
 
-const passwordSuggestions = document.querySelector('.password-suggestions')
-const passwordStrength = document.querySelector('.password-strength')
-const passwordCrackTime = document.querySelector('.password-time-to-crack')
+showPasswordButton.addEventListener('click',()=>{
+  const passwordShown = passwordInput.getAttribute('type') == 'text';
+  passwordInput.setAttribute('type', passwordShown ? 'password' : 'text');
+  showPasswordButton.innerHTML = passwordShown ? "<i class='ti ti-eye-closed'></i>" : "<i class='ti ti-eye'></i>";
 
-const scoreLookup = ["very weak", "very weak", "weak", "good", "strong"]
-
-const parsePassword = (password) =>{
-  let passwordEstimation = zxcvbn(password)
-  console.log(passwordEstimation)
-
-  passwordCharacters.innerText = password.length;
-
-  passwordStrength.innerText = scoreLookup[passwordEstimation.score]
-  passwordCrackTime.innerText = passwordEstimation.crack_times_display.offline_slow_hashing_1e4_per_second
-
-  if(passwordEstimation.feedback.suggestions.length != 0){
-    passwordSuggestions.innerHTML = passwordEstimation.feedback.suggestions.join("<br>");
-  }
-  else{
-    passwordSuggestions.innerText = "No suggestions for now!"
-  }
-}
-
-parsePassword("");
-
-passwordInput.addEventListener("input", (e)=>{
-  const password = e.target.value;
-  parsePassword(password)
 })
+passwordInput.addEventListener('input',()=>{
+  const password = passwordInput.value;
+  charCountLabel.innerText = password.length;
+  if(password.length > 0){
+    resultContainer.classList.remove("hidden");
+    const result = zxcvbn(password);
 
+    const scoreLookup = ["very weak", "very weak", "weak", "good", "strong"]
+    const score = scoreLookup[result.score];
+    scoreLabel.innerText = score;
+    if(!result.feedback.warning){
+      warningContainer.setAttribute('hidden','');
+    }else{
+      warningContainer.removeAttribute('hidden');
+    }
+    warningContainer.innerHTML = result.feedback.warning;
+    suggestionsLabel.innerHTML = result.feedback.suggestions.join(' ') || "No complaints...";
 
+    crackTimeLabel.innerText = result.crack_times_display.offline_slow_hashing_1e4_per_second;
+  }else{
+    resultContainer.classList.add("hidden");
+  }
+})
